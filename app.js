4d5016904e1bf386c30401c006f8089aa2a082a1
seigfred0 <saysonseigfred@gmail.com>
2023-09-09 21:07:20 +0800

    found the bug
    found the bug when I toggle between monthly and yearly billing

--- app.js
+++ app.js
@@ -1,22 +1,73 @@
 
+let pageViewsData = ["10K", "50K", "100K", "500K", "1M"];
+let perMonthData = [8, 12, 16, 24, 36];
+let slider = document.getElementById('price-slider');
+
+let pageView = document.getElementById('pageview');
+let pricePerMonth = document.getElementById('price');
+
+let billingToggle = document.getElementById('billing-toggle');
+let isYearly = false;
+
+slider.addEventListener('input', function() {
+    updateValue();
+
+    pageView.innerHTML = pageViewsData[slider.value];
+    // pricePerMonth.innerHTML = perMonthData[slider.value]; // found the culprit!!!
+
+
+    let value = this.value * 25;
+    this.style.background = `linear-gradient(to right,
+        hsl(174, 77%, 80%) 0%,
+        hsl(174, 77%, 80%) ${value}%,
+        hsl(224, 65%, 95%) 0%,
+        hsl(224, 65%, 95%) 100%)`;
+});
+
+billingToggle.addEventListener("change", function () {
+    if (isYearly == false) {
+      isYearly = true;
+    } else {
+      isYearly = false;
+    }
+    updateValue();
+});
+  
+function updateValue() {
+    if (isYearly) {
+      pricePerMonth.innerHTML = perMonthData[slider.value] * 0.75;
+    } else {
+      pricePerMonth.innerHTML = perMonthData[slider.value];
+    }
+}
 
 
 
+// CHECKING VARIABLE ///
+
+// function checkStatus() {
+//     if (isYearly) {
+//         console.log("It's true");
+//     } else {
+//         console.log("It's false");
+//     }
+// }
+
+// let intervalId = setInterval(checkStatus, 1000);
+
+// setTimeout(() => {
+//     isYearly = true;
+//   }, 5000);
 
 
-/*
-Here are the different page view ranges and the corresponding monthly price totals:
+// function updateValue() {
+//     let discountedPrice = perMonthData[slider.value] * 0.75;
+//     let standardPrice = perMonthData[slider.value];
 
-- 10K pageviews / $8 per month
-- 50K pageviews / $12 per month
-- 100K pageviews / $16 per month
-- 500k pageviews / $24 per month
-- 1M pageviews / $36 per month
+//     if (yearlyBilling) {
+//         pricePerMonth.innerHTML = discountedPrice;
+//     } else {
+//         pricePerMonth.innerHTML = standardPrice;
+//     }
+// }
 
-<div class="top-slider">
-    <input type="range" id="price-slider" min="0" max="4" value="2">
-</div>
-
-If the visitor switches the toggle to yearly billing, a 25% discount should be applied to all prices.
-
-*/
