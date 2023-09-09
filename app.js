0f8e166f71eb457093860c8ce19b74acbddc3805
seigfred0 <saysonseigfred@gmail.com>
2023-09-09 19:32:10 +0800

    CSS Done

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+/*
+Here are the different page view ranges and the corresponding monthly price totals:
+
+- 10K pageviews / $8 per month
+- 50K pageviews / $12 per month
+- 100K pageviews / $16 per month
+- 500k pageviews / $24 per month
+- 1M pageviews / $36 per month
+
+<div class="top-slider">
+    <input type="range" id="price-slider" min="0" max="4" value="2">
+</div>
+
+If the visitor switches the toggle to yearly billing, a 25% discount should be applied to all prices.
+
+*/
